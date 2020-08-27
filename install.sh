86b4aded9a5536dc0dedde746f0ab3a464f708da
Markus Daghall <markus@daghall.se>
2020-08-27 15:06:07 +0200

    Clean up install script

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 COLOR_YELLOW=$(echo -e "\033[1;33m")
 COLOR_GREEN=$(echo -e "\033[32m")
 COLOR_RED=$(echo -e "\033[31m")
+COLOR_BLUE=$(echo -e "\033[34m")
 COLOR_RESET=$(echo -e "\033[0m")
 
-# Function: Show unified diff
-function _diff() {
+function unified_diff() {
 	diff -u ~/$1 ./$1;
 }
 
 # Loop over relevant files and check for diffs, ask to apply diff if found
 for file in .bashrc .bash_profile .vimrc .gitconfig; do
-	diff=$(_diff $file);
+	diff=$(unified_diff $file);
 
 	# Is there a diff?
 	if [ -n "$diff" ]; then
@@ -20,12 +20,11 @@ for file in .bashrc .bash_profile .vimrc .gitconfig; do
 
 		# Print diff with pretty colors
 		echo -e $COLOR_YELLOW"Diff for $file:$COLOR_RESET"
-		_diff $file | head -2
-		_diff $file | tail -n +3 | sed -e "s/^-/$COLOR_RED-/" -e "s/^\+/$COLOR_GREEN+/" -e "s/$/$COLOR_RESET/"
+		unified_diff $file | sed -e "/^---/! s/^-/$COLOR_RED-/" -e "/^+++/! s/^\+/$COLOR_GREEN+/" -e "s/$/$COLOR_RESET/"
 
 		# Prompt for answer
 		while [[ ! $answer =~ ^[ynYN]$ ]]; do
-			read -p "Apply diff? [y/n] " answer;
+			read -p "${COLOR_BLUE}Apply diff? [y/n] ${COLOR_RESET}" answer;
 		done;
 
 		# Check answer
