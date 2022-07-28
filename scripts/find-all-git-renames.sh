3af823f8977d1b4aadf9857d03d0c7d77085ccfb
Markus Daghall <markus@daghall.se>
2022-07-28 22:54:51 +0200

    Fix tig blame "open file on GitHub" command
    tig's %(file) variable is populated with each row's
    filename at commit time, which is undesirable for
    this key-binding.
    
    This is fixed with a script that takes a file path
    as its only argument, and prints the current file path
    of that file, by stepping through its git renaming
    history, in reverse.

---
+++ scripts/find-all-git-renames.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Takes a file path as its only argument,
+# and prints the current file path of
+# that file, by stepping through its git history,
+# in reverse
+
+function DEBUG() {
+  [[ ! -z $DEBUG ]] && $@
+}
+
+filename=$1
+new_filename=$1
+
+while [[ $new_filename != "" ]]; do
+  last_commit=$(git log --format="%h" --follow -- $new_filename | head -1)
+  DEBUG echo "Last commit: $last_commit"
+
+  new_filename=$(git show $last_commit | grep -A1 "rename from $filename" | tail -1 | awk '{ print $NF }')
+  if [[ $new_filename != "" ]]; then
+    DEBUG echo "Renamed $filename => $new_filename"
+    filename=$new_filename
+  fi
+done
+
+echo $filename
