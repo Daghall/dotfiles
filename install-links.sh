5571299711c64ad5f976719047448c118b161e2d
Markus Daghall <markus@daghall.se>
2024-07-12 11:03:31 +0200

    Add files under ~/.config

--- install-links.sh
+++ install-links.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source ./scripts/bash_functions.sh
+
 for file_destination in \
   .bashrc \
   .bash_profile \
@@ -8,9 +10,21 @@ for file_destination in \
   .tigrc \
   .inputrc \
   .vim/bundle/vimspector/configurations/macos/_all/vimspector.json \
+  .config/ranger/rc.conf \
+  .config/gh/config.yml \
+  .config/gh/hosts.yml \
+  .config/karabiner/karabiner.json \
 ; do
-  filename=$(basename $file_destination)
-  echo -n "Linking $filename"
+
+  if [[ "$file_destination" =~ ^\.config/ ]]; then
+    filename=$(cut -d / -f2- <<< "$file_destination")
+  else
+    filename=$(basename $file_destination | sed 's#__#/#g')
+  fi
+
+  DEBUG_LOG "  $filename → ~/$file_destination"
+
+  printf "Linking %s" $filename
   ln -s $(pwd)/$filename ~/$file_destination 2>/dev/null
 
   if [[ $? -ne 0 ]]; then
@@ -20,5 +34,5 @@ for file_destination in \
       printf " (ERROR)"
     fi
   fi
-  echo ""
+  printf "\n"
 done
