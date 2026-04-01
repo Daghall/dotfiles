5887228c58f0efd75885538934c0306b56d4b57c
Markus Daghall <markus@daghall.se>
2026-04-01 17:02:19 +0200

    Only count successful runs that had a zero exit code

--- scripts/run-till-fail.sh
+++ scripts/run-till-fail.sh
@@ -12,8 +12,10 @@ successful_runs=0
 while [[ $exit_code -eq 0 ]]; do
   "$@"
   exit_code=$?
-  successful_runs=$(($successful_runs + 1))
+
+  if [[ $exit_code -eq 0 ]]; then
+    successful_runs=$(($successful_runs + 1))
+  fi
+
   stats
 done
-
-stats
